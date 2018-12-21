084c92c89d9e3194936f9bc6bbeee8f100f9134d
Andrew Qu <qua@rpi.edu>
2018-12-20 20:02:31 -0500

    added graph functionality

--- graph.py
+++ graph.py
@@ -3,8 +3,8 @@ Graph class
 
 '''
 
-from node import *
 from Edge import *
+
 class Graph:
 
     '''
@@ -30,14 +30,25 @@ class Graph:
         if(start not in self.__nodes):
             self.__nodes[start] = []
         self.__nodes[start].append(Edge(value,end))
+        self.checkRep()
 
     '''
     Gets neighbors of Node(node)
 
     @param node : node value to get neighbors of
+    @returns list of neighbors of Node(node)
     '''
     def getNeighbors(self, node):
         return self.__nodes[node]
+
+
+    '''
+    Lists all the nodes in the graph
+
+    @returns list of keys in nodes
+    '''
+    def listNodes(self):
+        return list(self.__nodes.keys())
 
     '''
     TODO: print representation
@@ -45,14 +56,30 @@ class Graph:
     def __str__(self):
         return ""
 
+    '''
+    TODO: shortest path from one node to another
+    given numerical edge labels
+    '''
+    def shortestPath(self, start, end):
+        return ""
+
+    '''
+    TODO: check representation to make sure types are consistent
+    ex: all edge weights are String and all nodes are Int
+    '''
+    def checkRep(self):
+        print("checking rep")
+
+
+
 '''
 Quick tests
 
 '''
-"""
+'''
 g = Graph()
 g.addEdge('a', 10, 'b')
 g.addEdge('a', 10, 'c')
 g.addEdge('a', 10, 'd')
 print(g.getNeighbors('a'))
-"""
+'''
